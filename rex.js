d14e6d0fdde9d766d9ec4a8a58bf450f90e24972
jean-luke <cabinhammer@gmail.com>
2018-01-18 19:10:55 +0000

    new index

--- rex.js
+++ rex.js
@@ -1,49 +1,49 @@
  $(function() {
-    $('#dialog').dialog({
+    $('#objectPage').dialog({
         autoOpen: false
     });
 });
 
 function goToBlogPage(blog)
 {
-    $('#dialog').dialog(
+    $('#objectPage').dialog(
         'option',
         'title',
         'blog ' + blog.ID + ': ' + blog.title
     );
-    $('#dialog').html(
+    $('#objectPage').html(
         '<p><b>summary: </b>' + blog.summary + '</p>' +
         '<p><b>text: </b>' + blog.text + '</p>' +
         '<br><p style="text-align:right">created by ' + blog.createdID + ' at ' + blog.dateCreated + '</p>'
     );
-    $('#dialog').dialog('open');
+    $('#objectPage').dialog('open');
 }
 	
 function goToEventPage(event)
 {
 	alert("rex over to you, event id="+event);
-    $('#dialog').dialog(
+    $('#objectPage').dialog(
         'option',
         'title',
         'event ' + event.ID + ': ' + event.title
     );
-    $('#dialog').html(
+    $('#objectPage').html(
         '<p><b>description: </b>' + event.description + '</p>' +
         '<p><b>summary: </b>' + event.summary + '</p>' +
         '<img src="' + event.image + '">' + 
         '<br><p style="text-align:right">created by ' + event.createdID + ' at ' + event.dateCreated + '</p>'
     );
-    $('#dialog').dialog('open');
+    $('#objectPage').dialog('open');
 }
 	
 function goToCampaignPage(campaign)
 {
-    $('#dialog').dialog(
+    $('#objectPage').dialog(
         'option',
         'title',
         'campaign ' + campaign.ID + ': ' + campaign.title
     );
-    $('#dialog').html(
+    $('#objectPage').html(
         '<p><b>description: </b>' + campaign.description + '</p>' +
         '<p><b>summary: </b>' + campaign.summary + '</p>' +
         '<p><b>target: </b>' + campaign.target + '</p>' +
@@ -51,6 +51,6 @@ function goToCampaignPage(campaign)
         '<p><b>expiry date: </b>' + campaign.expiryDate + '</p>' +
         '<br><p style="text-align:right">created by ' + campaign.createdID + ' at ' + campaign.dateCreated + '</p>'
     );
-    $('#dialog').dialog('open');
+    $('#objectPage').dialog('open');
 }
  
