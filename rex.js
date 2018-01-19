23d2aae4eeff4769162786b9c7cd81bed778b7c4
Jean-Luke <cabinhammer@gmail.com>
2018-01-19 02:14:10 +0000

    individual pages done

--- rex.js
+++ rex.js
@@ -1,10 +1,12 @@
- $(function() {
+/*  $(function() {
     $('#objectPage').dialog({
         autoOpen: false
     });
-});
+}); */
 
-function goToBlogPage(blog)
+var prevNewsfeed = "";
+
+/* function goToBlogPage(blog)
 {
     $('#objectPage').dialog(
         'option',
@@ -17,26 +19,67 @@ function goToBlogPage(blog)
         '<br><p style="text-align:right">created by ' + blog.createdID + ' at ' + blog.dateCreated + '</p>'
     );
     $('#objectPage').dialog('open');
+} */
+
+function goToBlogPage(blog)
+{
+	console.log("loading blog "+JSON.stringify(blog));
+
+    prevNewsfeed=document.getElementById("objectPage").innerHTML;
+	
+    setObjectPage(
+		"<div class='row'><div class='col-sm-3'></div><div class='col-sm-9'>"+"<button onclick='location.reload()'>Back</button>"+
+		"<h1>"+blog.title+"</h1><br>"+
+        '<p><b>text: </b>' + blog.text + '</p>' +
+        '<p><b>summary: </b>' + blog.summary + '</p>' +
+        '<img src="' + blog.image + '" style="height:400px;width:400px">' + 
+        '<br><p>created by ' + blog.creatorID + ' at ' + blog.dateCreated + '</p></div></div>'
+    );
+	
+    
 }
 	
 function goToEventPage(event)
 {
-	alert("rex over to you, event id="+event);
-    $('#objectPage').dialog(
-        'option',
-        'title',
-        'event ' + event.ID + ': ' + event.title
-    );
-    $('#objectPage').html(
+	console.log("loading event "+JSON.stringify(event));
+
+    prevNewsfeed=document.getElementById("objectPage").innerHTML;
+	
+    setObjectPage(
+		"<div class='row'><div class='col-sm-3'></div><div class='col-sm-9'>"+"<button onclick='location.reload()'>Back</button>"+
+		"<h1>"+event.title+"</h1><br>"+
         '<p><b>description: </b>' + event.description + '</p>' +
         '<p><b>summary: </b>' + event.summary + '</p>' +
-        '<img src="' + event.image + '">' + 
-        '<br><p style="text-align:right">created by ' + event.createdID + ' at ' + event.dateCreated + '</p>'
+        '<img src="' + event.image + '" style="height:400px;width:400px">' + 
+        '<br><p>created by ' + event.creatorID + ' at ' + event.dateCreated + '</p></div></div>'
     );
-    $('#objectPage').dialog('open');
+}
+
+function goToCampaignPage(campaign)
+{
+	console.log("loading campaign "+JSON.stringify(campaign));
+
+    prevNewsfeed=document.getElementById("objectPage").innerHTML;
+	
+    setObjectPage(
+		"<div class='row'><div class='col-sm-3'></div><div class='col-sm-9'>"+"<button onclick='location.reload()'>Back</button>"+
+		"<h1>"+campaign.title+"</h1><br>"+
+        '<p><b>description: </b>' + campaign.description + '</p>' +
+        '<p><b>summary: </b>' + campaign.summary + '</p>' +
+        '<img src="' + campaign.image + '" style="height:400px;width:400px">' + 
+        '<br><p>created by ' + campaign.creatorID + ' at ' + campaign.dateCreated + '</p></div></div><br><br>'
+		+ "<input type='number' id='donatedMoney'><span><button id='donate' onclick='donate()'>Donate!</button>"
+		+ '<iframe id="objectPage2" src= "objectPage.html"></iframe>'
+    );
+}
+
+function setObjectPage(htmlIn)
+{
+	console.log("setting object page as "+htmlIn)
+	$("#objectPage").html(htmlIn);
 }
 	
-function goToCampaignPage(campaign)
+/* function goToCampaignPage(campaign)
 {
     $('#objectPage').dialog(
         'option',
@@ -52,5 +95,5 @@ function goToCampaignPage(campaign)
         '<br><p style="text-align:right">created by ' + campaign.createdID + ' at ' + campaign.dateCreated + '</p>'
     );
     $('#objectPage').dialog('open');
-}
+} */
  
